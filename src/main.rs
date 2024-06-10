022ca3756d4f0da6b21524b77ab4897f5f196faa
mdabir1203 <mdabir1203@gmail.com>
2024-06-11 03:55:51 +0500

    password derivation

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,34 @@ use ring::rand::SecureRandom;
 use ring::{digest, pbkdf2};
 use std::num::NonZeroU32;
 
+
+// main returns result -> handle potential errors
+
+fn main() -> Result<(), Unspecified> {
+    const CREDENTIAL_LEN: usize = digest::SHA512_OUTPUT_LEN; // 64 bytes
+    let n_iter = NonZeroU32::new(100000).unwrap();
+    let rng = SystemRandom::new();
+
+    // Generation of salt 
+    // salt array of 64 bytes created and filled with secure random bytes
+    let mut salt = [0u8; CREDENTIAL_LEN];
+    rng.fill(&mut salt)?;
+    
+    // Pasword derivation
+    // 
+    let password = "Who let the dogs out ?";
+    let mut store_hash = [0u8; CREDENTIAL_LEN];
+    pbkdf2::derive(
+        pbkdf2::PBKDF2_HMAC_SHA512,
+        n_iter,
+        &salt,
+        password.as_bytes(),
+        &mut store_hash,
+    );
+
+    println!("Salt: {}", HEXUPPER.encode(&salt));
+    println!("Stored Hash: {}", HEXUPPER.encode(&store_hash));
+
+
+
+}
